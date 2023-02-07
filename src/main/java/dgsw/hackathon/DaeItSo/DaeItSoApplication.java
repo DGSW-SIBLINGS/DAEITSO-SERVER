8bfa163ad996500fdb5fdd624ed72ff10f65750d
이지민 <wlals060330@gmail.com>
2023-02-07 09:27:07 +0900

    :tada: : init

---
+++ src/main/java/dgsw/hackathon/DaeItSo/DaeItSoApplication.java
@@ -0,0 +1,13 @@
+package dgsw.hackathon.DaeItSo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeItSoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaeItSoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dgsw/hackathon/DaeItSo/DaeItSoApplicationTests.java
@@ -0,0 +1,13 @@
+package dgsw.hackathon.DaeItSo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaeItSoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
