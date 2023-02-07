1cd6a5cefe043f6d4ba1cd7903f8080fd1a80468
이지민 <wlals060330@gmail.com>
2023-02-07 19:33:51 +0900

    :sparkles: : add Enum Exception

--- src/main/java/dgsw/hackathon/DaeItSo/global/handler/GlobalExceptionHandler.java
+++ src/main/java/dgsw/hackathon/DaeItSo/global/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import dgsw.hackathon.DaeItSo.global.error.ErrorCode;
 import dgsw.hackathon.DaeItSo.global.error.ErrorResponseEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,12 +14,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomError.class)
-    protected ResponseEntity handleCustomException(CustomError e){
+    protected ResponseEntity handleCustomException(CustomError e) {
         return ErrorResponseEntity.responseEntity(e.getErrorCode());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity handleNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity
+                .status(404)
+                .body(ErrorResponseEntity.builder()
+                        .status(ErrorCode.NOT_FOUND.getHttpStatus().value())
+                        .code(ErrorCode.NOT_FOUND.name())
+                        .message(e.getMessage())
+                        .build());
+    }
+
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity handleException(Exception e){
+    protected ResponseEntity handleException(Exception e) {
         log.error(e.toString());
         return ResponseEntity
                 .status(500)
